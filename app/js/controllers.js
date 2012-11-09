961bb8c8b60bd892de3d6f4ca77fdae001c9c983
cliftonc <clifton.cunningham@gmail.com>
2012-11-09 18:05:13 +0000

    Mostly working

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,9 +2,10 @@
 
 
 /* Controllers */
-function ChannelColumnCtrl($scope, $route, $routeParams, $location, $http, $rootScope, ChannelService) {
+function ChannelColumnCtrl($scope, $route, $routeParams, $timeout, $location, $http, $rootScope, ChannelService) {
 
-	$scope.channels = $rootScope.channels;
+	// Default View
+	// $scope.channels = $rootScope.channels;
 
 	$scope.openArticle = function() {
 
@@ -36,8 +37,10 @@ function ChannelColumnCtrl($scope, $route, $routeParams, $location, $http, $root
 	}
 
 	// Listen for first instantiation
-	$scope.$on("render", function(e) {		
-		$scope.channels = $rootScope.channels;
+	$scope.$on("render", function(e) {			
+		console.log('on channel render');		
+		$scope.channels = $rootScope.channels;			
+		$timeout(function() { $scope.$root.$eval() }, 200)
 	});
 
 	$scope.$on("channelUpdated", function(e) {
@@ -49,63 +52,36 @@ function ArticleCtrl($scope, $route, $compile, $routeParams, $rootScope, $http) 
 	
 	var channel = $routeParams.channel,
 		articleId = $routeParams.articleId;
-
-	$scope.imageClick = function() {
-		console.log('clicker')
-	}
-
-	function getImage(article, id) {		
-
-		console.log('get image for ' + id);
-		var image = _.find(article.aImages, function(image) {
-			if(image.id == id) return image;
-		})
-		return '//i.mol.im' + image.url;
-
-	}
-
-	function processText(article) {
-		
-		var processedTextDom = $('<div>' + article.aTxt + '</div>'),
-			imageBoxes = $('.imgBox', processedTextDom);
-
-		_.map(imageBoxes, function(imageBox) {
-
-			var link = $('a', imageBox),
-				img = $('a img', imageBox);
-
-			link.attr('ng-click',link.attr('href').replace('dmapp:','').replace(':','(\'') + '\')');
-			link.attr('href',null);
-			img.attr('ng-src', getImage(article, img.attr('id').replace('/','')));
-			img.attr('id', null);
-
-		});
-
-		return processedTextDom.html();
-	}
+	
+    $scope.imgClick = function(imageId) {
+      console.log('clicker ' + imageId);
+      return false;
+    }
 
 	function render() {
 		var articles = $rootScope.channels[$rootScope.channelSettingsData[$routeParams.channel].dataIndex].items;
 		_.each(articles, function(article) {
-			if(article.aId == articleId) {
-				// We unfortunately need to process the article text				
+			if(article.aId == articleId) {									
 				$scope.article = article;
-				$('.x-innerhtml').append($compile(processText(article))($scope));
 			}
 		})
 	}
 
 	// If we already have data
-	if($rootScope.channels) render();
+	if($rootScope.channels) {
+		console.log('have channel data')
+		render();
+	}
 
-	// Listen for first instantiation
+	// Listen for first instantiation - load on articel page
 	$scope.$on("render", function(e) {
+		console.log('on article render');
 		render();
 	});
 	
 }
 
-function ChannelNavigationCtrl($scope, $route, $routeParams, $http, $rootScope, Settings, ChannelService) {
+function ChannelNavigationCtrl($scope, $route, $routeParams, $http, $rootScope, $timeout, Settings, ChannelService) {
 
 	// Display the loading image
 	$scope.loadingImage = 'img/loading/Default-Landscape.png';
@@ -135,6 +111,7 @@ function ChannelNavigationCtrl($scope, $route, $routeParams, $http, $rootScope, 
 	$scope.$on('serverSettingsChanged', function(e, data) {
 
 		// This means that we now have *NEW* settings from the server
+		console.log('we have new server settings');
 
 	});	
 
@@ -143,8 +120,6 @@ function ChannelNavigationCtrl($scope, $route, $routeParams, $http, $rootScope, 
 
 		// if we don't have both yet exit
 		if(!$rootScope.settings || !$rootScope.userSettings) return;
-
-		console.log($rootScope.settings.channels);
 
 		async.map($rootScope.settings.channels, 
 
@@ -160,11 +135,13 @@ function ChannelNavigationCtrl($scope, $route, $routeParams, $http, $rootScope, 
 					ChannelService.get(channel.sn, function(err, data) {	
 						
 						data.sn = channel.sn; // copy sn into channel
+
 						$rootScope.channelSettingsData[channel.sn].dataIndex = $rootScope.channels.length;										
 						$rootScope.channelSettingsData[channel.sn].enabled = true;										
-
 						$rootScope.channels.push(data);
+
 						$('.loadingStatus').append(channel.sn + ' ');
+
 						cb();
 
 					});		
@@ -173,16 +150,18 @@ function ChannelNavigationCtrl($scope, $route, $routeParams, $http, $rootScope, 
 
 					$rootScope.channelSettingsData[channel.sn].enabled = false;	
 					cb();
+
 				}
 
 			}, function(err) {
 
 				// Draw the channel navigation
-				$scope.loadingImage = null;
 				$scope.channelSettings = $rootScope.settings.channels;	
 
 				// broadcast that we should render channels
-				$rootScope.$broadcast("render");
+				$rootScope.$broadcast("render");				
+
+				$timeout(function() { $scope.loadingImage = null }, 300);
 							
 			});
 
@@ -191,41 +170,20 @@ function ChannelNavigationCtrl($scope, $route, $routeParams, $http, $rootScope, 
 
 	$scope.changeChannel = function() {
 
-		console.log(this.channel.sn + ' = ' + this.channel.enabled);
-		Settings.update.SetChannel(this.channel.sn, this.channel.enabled, function() {
+		var self = this;
+		Settings.update.SetChannel(self.channel.sn, self.channel.enabled, function() {
+
+			//if(!self.channel.enabled) {
+			//	// Remove it				
+			//	$rootScope.channels.splice($rootScope.channelSettingsData[self.channel.sn].dataIndex, 1);				
+		///	}
+
 			$rootScope.$broadcast("render");	
+
 		})		
 
 	}
 
 	Settings.get('GB');
-
-	/*, function(err, data) {
-		
-		
-		
-		// Broadcast to enable channels to be requested
-		async.map(data.subscribedChannels, 
-			function(channel, cb) {			
-				$rootScope.channelSettingsData[channel.sn] = channel;			
-				ChannelService.get(channel.sn, function(err, data) {	
-					data.sn = channel.sn; // copy sn into channel
-					$rootScope.channelSettingsData[channel.sn].dataIndex = $rootScope.channels.length;										
-					$rootScope.channels.push(data);
-					$('.loadingStatus').append(channel.sn + ' ');
-					cb();
-				});		
-			}, function(err) {
-
-				// Draw the channel navigation
-				$scope.loadingImage = null;
-				$scope.channelSettings = data.subscribedChannels;	
-
-				// broadcast
-				$rootScope.$broadcast("render");		
-							
-			});
-
-	});*/
         
 }
--- app/js/directives.js
+++ app/js/directives.js
@@ -3,23 +3,70 @@
 /* Directives */
 
 
-angular.module('myApp.directives', []).
-  directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
-    };
-  }])
+angular.module('myApp.directives', []).directive('ngFullHeight', function() {
+    return function(scope, element, attrs) {               
+      $(".channel-table").css("height", '1100px !important');
+    }
+  }) 
   .directive('ngTap', function() {
   	return function(scope, element, attrs) {
   		var tapping = false;
     	element.bind('touchstart', function() {
-      		tapping = true;
+      	 tapping = true;
     	});
     	element.bind('touchmove', function() {
-			tapping = false;
+			   tapping = false;
     	});
     	element.bind('touchend', function() {
-      		if(tapping) scope.$apply(attrs['ngTap']);
+      	 if(tapping) scope.$apply(attrs['ngTap']);
     	});
   	};
+  })
+  .directive('articleHtml', function($compile, $rootScope) {
+ 
+    function getImage(article, id) {    
+
+      //console.log('get image for ' + id);
+      var image = _.find(article.aImages, function(image) {
+        if(image.id == id) return image;
+      })
+      return '//i.mol.im' + image.url;
+
+    }
+
+    function processText(article) {
+
+      if(!article) return;
+    
+      var processedTextDom = $('<div>' + article.aTxt + '</div>'),
+        imageBoxes = $('.imgBox', processedTextDom);
+
+      _.map(imageBoxes, function(imageBox) {
+
+        var link = $('a', imageBox),
+            img = $('a img', imageBox);
+
+        link.attr('ng-click', link.attr('href').replace('dmapp:','').replace(':','(\'') + '\')');
+        link.removeAttr('href');
+
+        img.attr('ng-src', getImage(article, img.attr('id').replace('/','')));
+        img.removeAttr('id');
+
+      });
+
+      return processedTextDom.html();
+
+    }
+
+     return function(scope, elm, attrs) {
+
+        scope.$watch(attrs.articleHtml, function(newValue, oldValue) {
+
+            elm.html(processText(newValue));
+            $compile(elm.contents())(scope);
+
+        });
+    };
+
   });
+   
